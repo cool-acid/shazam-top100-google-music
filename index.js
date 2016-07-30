7d55a0118fbb962d6ddd6a0bac0b2c7b0afb7323
Cool Acid <coolacid217@gmail.com>
2016-07-30 00:17:24 -0500

    Initializing playmusic

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const rp = require('request-promise-native')
+const PlayMusic = require('playmusic')
+const pm = new PlayMusic()
+const config = require('./config')
 
-const rpOptions = {
-  uri: 'http://cdn.shazam.com/shazam/v2/en/MX/android/-/tracks/web_chart_world',
-  json: true,
-}
-
-rp(rpOptions)
+// Initializing PlayMusic
+pm.init(config.google, err => {
+  if (err) {
+    console.log(err)
+    return
+  }
+  // Getting Shazam JSON
+  const rpOptions = {
+    uri: 'http://cdn.shazam.com/shazam/v2/en/MX/android/-/tracks/web_chart_world',
+    json: true,
+  }
+  rp(rpOptions)
   .then( json => {
     console.log(json.chart.length)
     if (json.chart.length) {
@@ -23,3 +32,4 @@ rp(rpOptions)
   .catch( err => {
     console.log(err)
   })
+})
